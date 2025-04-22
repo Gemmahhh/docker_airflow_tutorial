fe95663b5d637c7df42e3c86a4f2a942a423dac4
Gemmahhh <okpeamaka8@gmail.com>
2025-04-22 21:48:18 +0100

    correction

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -8,7 +8,8 @@ default_args ={
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(name, age):
+def greet(age, ti):
+    name = ti.xcom_pull(task_ids = 'get_name')
     print("Hello World! My name is {name} and I am {age} years old")
 
 def get_name():
@@ -16,18 +17,19 @@ def get_name():
 
 with DAG(
     default_args= default_args,
-    dag_id = 'our_dag_with_python_operator_v03',
+    dag_id = 'our_dag_with_python_operator_v04',
     description= 'Our first dag using Python Operator',
     start_date= datetime(2025,4, 20),
     schedule_interval= '@daily'
 ) as dag:
-    # task1 = PythonOperator(
-    #     task_id = 'greet',
-    #     python_callable= greet,
-    #     op_kwargs={'name': 'Chiamaka', 'age': 25}
-    # )
+    task1 = PythonOperator(
+        task_id = 'greet',
+        python_callable= greet,
+        op_kwargs={'age': 25}
+    )
     task2 = PythonOperator(
         task_id = 'get_name',
         python_callable= get_name
     )
-    task2
+    # this means that task 1 will run after task 2 has been completed. 
+    task2 >> task1
