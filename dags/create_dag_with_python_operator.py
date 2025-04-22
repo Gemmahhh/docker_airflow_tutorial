b65d270e77248dd12e37a51d9b30cfe3901d45af
Gemmahhh <okpeamaka8@gmail.com>
2025-04-22 21:29:17 +0100

    Returning a function with the python operator

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -11,16 +11,23 @@ default_args ={
 def greet(name, age):
     print("Hello World! My name is {name} and I am {age} years old")
 
+def get_name():
+    return 'Chiamaka'
+
 with DAG(
     default_args= default_args,
-    dag_id = 'our_dag_with_python_operator_v02',
+    dag_id = 'our_dag_with_python_operator_v03',
     description= 'Our first dag using Python Operator',
     start_date= datetime(2025,4, 20),
     schedule_interval= '@daily'
 ) as dag:
-    task1 = PythonOperator(
-        task_id = 'greet',
-        python_callable= greet,
-        op_kwargs={'name': 'Chiamaka', 'age': 25}
+    # task1 = PythonOperator(
+    #     task_id = 'greet',
+    #     python_callable= greet,
+    #     op_kwargs={'name': 'Chiamaka', 'age': 25}
+    # )
+    task2 = PythonOperator(
+        task_id = 'get_name',
+        python_callable= get_name
     )
     task1
