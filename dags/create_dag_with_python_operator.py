6bd0363f0a52311ce0f84f5764b96a434c24f324
Gemmahhh <okpeamaka8@gmail.com>
2025-04-22 22:00:47 +0100

    correction

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -9,27 +9,30 @@ default_args ={
 }
 
 def greet(age, ti):
-    name = ti.xcom_pull(task_ids = 'get_name')
-    print("Hello World! My name is {name} and I am {age} years old")
+    first_name = ti.xcom_pull(task_ids = 'get_name_and_age', key = 'first_name')
+    last_name = ti.xcom_pull(task_ids = 'get_name_and_age', key = 'last_name')
+    last_name = ti.xcom_pull(task_ids = 'get_name_and_age', key = 'age')
+    print("Hello World! My name is {first_name} {last_name}, and I am {age} years old")
 
-def get_name():
-    return 'Chiamaka'
+def get_name_and_age(ti):
+    ti.xcom_push(key = 'first_name', value = 'Chiamaka')
+    ti.xcom_push(key = 'last_name', value = 'Okpe')
+    ti.xcom_push(key = 'age', value = 25)
 
 with DAG(
     default_args= default_args,
-    dag_id = 'our_dag_with_python_operator_v04',
+    dag_id = 'our_dag_with_python_operator_v05',
     description= 'Our first dag using Python Operator',
     start_date= datetime(2025,4, 20),
     schedule_interval= '@daily'
 ) as dag:
     task1 = PythonOperator(
         task_id = 'greet',
-        python_callable= greet,
-        op_kwargs={'age': 25}
+        python_callable= greet
     )
     task2 = PythonOperator(
-        task_id = 'get_name',
-        python_callable= get_name
+        task_id = 'get_name_and_age',
+        python_callable= get_name_and_age
     )
     # this means that task 1 will run after task 2 has been completed. 
     task2 >> task1
