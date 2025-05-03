bc4c0468e090a34190fe7016ba764da2e154d3a4
Gemmahhh <okpeamaka8@gmail.com>
2025-05-03 08:09:13 +0100

    Dag with cron expression

---
+++ dags/dag_with_cron_expression.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+from airflow import DAG 
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'Chiamaka',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id = 'dag_with_cron_expression_v1',
+    default_args= default_args,
+    description='This is our first dag with cron expression',
+    start_date=datetime(2025,5,1),
+    schedule_interval= '0 0 * * *',
+    catchup= False
+) as dag:
+    task1 = BashOperator(
+        task_id = 'first_task',
+        bash_command="echo This is a dag with cron expression!"
+    )
